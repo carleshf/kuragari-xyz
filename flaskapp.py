ace7a2dca5f14878392bbfb3c95d1890b488da39
Carles Hernandez-Ferrer <2046180+carleshf@users.noreply.github.com>
2024-07-05 17:00:35 +0200

    Added language as get parameter. Updated lang badges to reflect disponibility.

--- flaskapp.py
+++ flaskapp.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, request, render_template
 
 from datetime import datetime
 
-data_path = '/home/carleshf/kuragari-xyz/data'
+data_path = 'C:/Users/carle/Documents/GitHub/kuragari-xyz/data'
 
 from data import load_labels, load_modules, load_events, get_lang, set_lang, labels_lang, display_modules_in_rows, get_module
 
@@ -23,29 +23,34 @@ def inject_now():
     return { 'now': datetime.utcnow() }
 
 
+"""
 @app.route( '/language' )
 def language():
     lang  = request.args.get( 'lang', default = get_lang(), type = str )
     set_lang( lang )
     return redirect( '/catalogue', code = 302 )
+"""
 
 
 @app.route( '/' )
 @app.route( '/catalogue' )
 def catalogue():
-    return render_template( 'index.html', labels = labels_lang( lbls, get_lang() ), plys = display_modules_in_rows( mdls, 3, get_lang() ) )
+    set_lang( request.args.get( 'lang', default = get_lang(), type = str ) )
+    return render_template( 'index.html', labels = labels_lang( lbls, get_lang() ), plys = display_modules_in_rows( mdls, 3, get_lang() ), lang = get_lang() )
   
 
 @app.route( '/module' )
 def module():
     title = request.args.get( 'title', default = '*', type = str )
     mld = get_module( mdls, title )
-    return render_template( 'module.html', labels = labels_lang( lbls, get_lang() ), mld = mld )
+    set_lang( request.args.get( 'lang', default = get_lang(), type = str ) )
+    return render_template( 'module.html', labels = labels_lang( lbls, get_lang() ), mld = mld, lang = get_lang() )
 
 
 @app.route( '/events' )
 def events():
-    return render_template( 'events.html', labels = labels_lang( lbls, get_lang() ), events = evts )
+    set_lang( request.args.get( 'lang', default = get_lang(), type = str ) )
+    return render_template( 'events.html', labels = labels_lang( lbls, get_lang() ), events = evts, lang = get_lang() )
 
 
 if __name__ == '__main__':
